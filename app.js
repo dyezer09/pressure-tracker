cab53589f93d3056f11482a85e1bb66394f991cb
dyezer09 <den07y@gmail.com>
2024-02-20 20:34:58 +0300

    add: express

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from "express" // get express
+import path from "path"
+import { fileURLToPath } from "url";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url) // get path to app.js
+const __dirname = path.dirname(__filename) // path to app.js whithout app.js
+const port = 5500;
+
+app.use(express.static(path.join(__dirname, ""))) // mount path to static
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/pages/index.html");
+});
+
+app.get("/all", (req, res) => {
+  res.sendFile(__dirname + "/pages/all.html");
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ readme.md
@@ -0,0 +1,13 @@
+# i dont know
+
+
+
+----TODO:----
+
+git ch -b express
+5. pnpm i express
+- edit: app.js replace node:http with express [https://expressjs.com/en/starter/hello-world.html]
+- add: pages to ./views & create route (app.js)
+- fix: path to css [https://stackoverflow.com/questions/5924072/express-js-cant-get-my-static-files-why]
+==commit==
+6. edge layout
