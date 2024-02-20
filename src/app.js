1d2b9967d7d1df84de2cb48a268d2a1250fc03f7
dyezer09 <den07y@gmail.com>
2024-02-20 21:38:32 +0300

    add: handlebars

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 import express from "express" // get express
 import path from "path"
 import { fileURLToPath } from "url";
+import { engine } from 'express-handlebars';
+
+
+
 const app = express();
 const __filename = fileURLToPath(import.meta.url) // get path to app.js
 const __dirname = path.dirname(__filename) // path to app.js whithout app.js
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.resolve(__dirname,'./views'));
 const port = 5500;
 app.use(express.static(path.join(__dirname, "../")))
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/pages/index.html");
+  res.render('./layouts/main');
+  //res.sendFile(__dirname + "/pages/index.html");
 });
 
 app.get("/all", (req, res) => {
